059ddcd328f45312ba5f554716ed613cbbcd9fff
JPPorcel <juanpiporcel@gmail.com>
2021-12-11 20:01:07 +0000

    Landing

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './assets/css/main.css';
+
+function App() {
+    return (
+        <div id="wrapper">
+            <div id="bg"></div>
+            <div id="main">
+
+                <header id="header">
+                    <h1>Juan Pablo Porcel</h1>
+                    <p>Full stack developer &nbsp;&bull;&nbsp; Tenerife &nbsp;&bull;&nbsp; Granada</p>
+                    <nav>
+                        <ul>
+                            <li><a href="https://github.com/JPPorcel" class="icon brands fa-github"><span class="label">Github</span></a></li>
+                            <li><a href="mailto:me@jpporcel.dev" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+                        </ul>
+                    </nav>
+                </header>
+
+                <footer id="footer">
+                </footer>
+
+            </div>
+        </div>
+    );
+}
+
+export default App;
